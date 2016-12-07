d14056f8fcbb24853c69574614246d47f88fd826
r2fresh <r2fresh78@gmail.com>
2016-12-07 17:54:18 +0900

    menu page 개발중

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -1,13 +1,18 @@
 define([
    'module',
    'text!tpl/menu.html',
-   'js/Model'
+   'js/Model',
+   'data/menu.js'
    ],
-   function(module, Menu, Model){
+   function(module, Menu, Model, MenuData){
 
 	'use strict'
 
  	module.exports = new (Backbone.View.extend({
+
+        events :{
+            'click .kt_menu_serarch_btn': 'onClickSearch',
+ 		},
 
         render:function(){
 
@@ -37,7 +42,120 @@ define([
 
         },
 
+        onClickSearch:function(){
+
+
+            //var kkk = '[{"Vehicle":"BMW","Date":"30, Jul 2013 09:24 AM","Location":"Hauz Khas, Enclave, New Delhi, Delhi, India","Speed":42},{"Vehicle":"Honda CBR","Date":"30, Jul 2013 12:00 AM","Location":"Military Road,  West Bengal 734013,  India","Speed":0},{"Vehicle":"Supra","Date":"30, Jul 2013 07:53 AM","Location":"Sec-45, St. Angel\'s School, Gurgaon, Haryana, India","Speed":58},{"Vehicle":"Land Cruiser","Date":"30, Jul 2013 09:35 AM","Location":"DLF Phase I, Marble Market, Gurgaon, Haryana, India","Speed":83},{"Vehicle":"Suzuki Swift","Date":"30, Jul 2013 12:02 AM","Location":"Behind Central Bank RO, Ram Krishna Rd by-lane, Siliguri, West Bengal, India","Speed":0},{"Vehicle":"Honda Civic","Date":"30, Jul 2013 12:00 AM","Location":"Behind Central Bank RO, Ram Krishna Rd by-lane, Siliguri, West Bengal, India","Speed":0},{"Vehicle":"Honda Accord","Date":"30, Jul 2013 11:05 AM","Location":"DLF Phase IV, Super Mart 1, Gurgaon, Haryana, India","Speed":71}]'
+
+            var kkk = '[{"페이지명":"DOWN","2016-12-01":100,"2016-12-02":200},{"페이지명":"DOWN/LTE","2016-12-01":300,"2016-12-02":400},{"페이지명":"DOWN/안녕하세요","2016-12-01":300,"2016-12-03":400}]'
+
+            this.test(kkk,"메뉴별&nbsp&nbsp통계", true)
+
+        },
+
+
+
+
+
+
+
+
+
+        test:function(JSONData, ReportTitle, ShowLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+    var CSV = '';
+    //Set Report title in first row or line
+
+    CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {
+        alert("Invalid data");
+        return;
+    }
+
+    //Generate a file name
+    var fileName = "MyReport_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");
+
+    //Initialize file format you want csv or xls
+    //var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    var uri = 'data:text/csv;charset=UTF-8,\uFEFF' + encodeURI(CSV);
+
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         getMenu:function(){
+
+            this.getMenuSuccess(MenuData);
+            return;
 
             var token = store.get('auth').token;
 
@@ -55,7 +173,38 @@ define([
             })
         },
         getMenuSuccess:function(data, textStatus, jqXHR){
+
+            console.log("sdfdf")
+
             console.log(data);
+
+
+
+            // var menuDataArr = [];
+            //
+            // var menuKeyList = _.keys( data.menuStatisticMap );
+            //
+            // for(var i=0; i<menuKeyList.length; ++i){
+            //
+            //     var keyList = _.keys( data.menuStatisticMap[menuKeyList[i]] )
+            //
+            //     var list = [];
+            //
+            //     for(var j=0; j<keyList.length; ++j){
+            //
+            //         list.push( data.menuStatisticMap[menuKeyList[i]][keyList[j]] )
+            //
+            //     }
+            //
+            //     var obj = {
+            //         'list' : list,
+            //         'menu' : menuKeyList[i],
+            //     }
+            //     menuDataArr.push(obj)
+            // }
+            //
+            // console.log( menuDataArr )
+
         },
         getMenuError:function(jsXHR, textStatus, errorThrown){
 
